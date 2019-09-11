fac106cf43f9399c99166d215b002ec01a3f11ed
Charlie Oster <oster.charlie@yahoo.com>
2019-09-11 21:22:27 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,2 @@
-# Use this space to code along with the readme
+ruby greeting.rb   
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
